49660eef56e0c7b5729d22c26490cc22a9e68088
Sagnik Sarkar <sagnik106@gmail.com>
2019-10-19 07:58:21 +0530

    1st hour

---
+++ edusmart.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+from pytube import YouTube
+class edusmart:
+    def __init__(self, m):
+        pass
+    def takeNotes(self,file_name='temp.txt'):
+        pass
+    def annotateNotes(self,file):
+        pass
+    def youtubeNotes(self,url):
+        pass
